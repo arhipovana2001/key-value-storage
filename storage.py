f4af2b1b204c7c20a1a9bc94726cac1400e9e2bd
Nastya Arkhipova <55373084+arhipovana2001@users.noreply.github.com>
2020-09-14 23:31:24 +0700

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,17 +1,67 @@
-import argparse, os, tempfile, json
+import argparse
+import os
+import tempfile
+import json
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--key', help='key', type=str)  # добавлена опция добавления ключа
-parser.add_argument('--val', help='val', type=str, default=None)  # добавлена опция добавления значения
-args = parser.parse_args()  # # Namespace(key='key', val=None)
-key = args.key
-val = args.val
-my_dict = {}
-
-print(1, args)
-print(2, key)
-print(3, val)
+def update_dict(dict, key, value):
+    if key not in dict:
+        dict[key] = value
+    else:
+        new_value = [dict[key]]
+        new_value.append(value)
+        dict[key] = new_value
+    return dict
 
 
-storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+def parsing():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--key', help='key', type=str)  # добавлена опция добавления ключа
+    parser.add_argument('--val', help='val', type=str, default=None)  # добавлена опция добавления значения
+    args = parser.parse_args()  # Namespace(key='key', val=None)
+    key = args.key
+    val = args.val
+    return key, val
+
+
+def main():
+    key, val = parsing()
+    storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+    if os.path.isfile('storage.data') is True:
+        #print("ФАЙЛ ЕСТЬ")
+        # передан только ключ
+        if val is None:
+            with open('storage.data', 'r') as file:
+                data = json.load(file)
+                if type(data[key]) is list:
+                    data = [str(i) for i in data[key]]
+                    print(', '.join(data))
+                else:
+                    print(data[key])
+
+        # переданы ключ и значение
+        elif val is not None and key is not None:
+            # считываем данные из файла
+            with open('storage.data', 'r') as file:
+                data = json.load(file)
+                data = update_dict(data, key, val)
+            with open('storage.data', 'w') as file:
+                json.dump(data, file)
+
+    else:
+        # первая запись в файл
+        with open('storage.data', 'w') as file:
+            json.dump(dict(), file)
+
+        with open('storage.data', 'r') as file:
+            data = json.load(file)  # загрузили данные из файла
+            data = update_dict(data, key, val)  # обновили данные
+
+        with open('storage.data', 'w') as file:  # добавили данные в файл
+            json.dump(data, file)
+
+        if val is None:
+            print(None)
+
+
+main()
