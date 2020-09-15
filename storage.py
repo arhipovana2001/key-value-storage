cb033331c657722a5567497cc9e3e8e0fa9f47ae
Nastya Arkhipova <55373084+arhipovana2001@users.noreply.github.com>
2020-09-15 17:33:26 +0700

    Update storage.py

--- storage.py
+++ storage.py
@@ -26,42 +26,45 @@ def parsing():
 
 def main():
     key, val = parsing()
-    storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
-    if os.path.isfile('storage.data') is True:
-        #print("ФАЙЛ ЕСТЬ")
-        # передан только ключ
-        if val is None:
-            with open('storage.data', 'r') as file:
-                data = json.load(file)
-                if type(data[key]) is list:
-                    data = [str(i) for i in data[key]]
-                    print(', '.join(data))
-                else:
-                    print(data[key])
+        if key == 'None' or key == 'none':
+        print('ERROR. KEY DOES NOT BE None')
+    else:
+        storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+        if os.path.isfile('storage.data') is True:
+            #print("ФАЙЛ ЕСТЬ")
+            # передан только ключ
+            if val is None:
+                with open('storage.data', 'r') as file:
+                    data = json.load(file)
+                    if type(data[key]) is list:
+                        data = [str(i) for i in data[key]]
+                        print(', '.join(data))
+                    else:
+                        print(data[key])
 
-        # переданы ключ и значение
-        elif val is not None and key is not None:
-            # считываем данные из файла
-            with open('storage.data', 'r') as file:
-                data = json.load(file)
-                data = update_dict(data, key, val)
+            # переданы ключ и значение
+            elif val is not None and key is not None:
+                # считываем данные из файла
+                with open('storage.data', 'r') as file:
+                    data = json.load(file)
+                    data = update_dict(data, key, val)
+                with open('storage.data', 'w') as file:
+                    json.dump(data, file)
+
+        else:
+            # первая запись в файл
             with open('storage.data', 'w') as file:
+                json.dump(dict(), file)
+
+            with open('storage.data', 'r') as file:
+                data = json.load(file)  # загрузили данные из файла
+                data = update_dict(data, key, val)  # обновили данные
+
+            with open('storage.data', 'w') as file:  # добавили данные в файл
                 json.dump(data, file)
 
-    else:
-        # первая запись в файл
-        with open('storage.data', 'w') as file:
-            json.dump(dict(), file)
-
-        with open('storage.data', 'r') as file:
-            data = json.load(file)  # загрузили данные из файла
-            data = update_dict(data, key, val)  # обновили данные
-
-        with open('storage.data', 'w') as file:  # добавили данные в файл
-            json.dump(data, file)
-
-        if val is None:
-            print(None)
+            if val is None:
+                print(None)
 
 
 main()
